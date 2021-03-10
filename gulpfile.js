53591d13b28b37eb7fb167326c6e94cc0d68d6d8
Xavier <tusaromix@gmail.com>
2021-03-10 19:32:57 +0100

    Version Miercoles 10/03/21

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-const { src, dest, watch , parallel } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('autoprefixer');
-const postcss    = require('gulp-postcss')
+const postcss = require('gulp-postcss')
 const sourcemaps = require('gulp-sourcemaps')
 const cssnano = require('cssnano');
 const concat = require('gulp-concat');
@@ -11,7 +11,6 @@ const imagemin = require('gulp-imagemin');
 const notify = require('gulp-notify');
 const cache = require('gulp-cache');
 const webp = require('gulp-webp');
-
 const paths = {
     scss: 'src/scss/**/*.scss',
     js: 'src/js/**/*.js',
@@ -26,40 +25,40 @@ function css() {
         .pipe(postcss([autoprefixer(), cssnano()]))
         // .pipe(postcss([autoprefixer()]))
         .pipe(sourcemaps.write('.'))
-        .pipe( dest('./build/css') );
+        .pipe(dest('./build/css'));
 }
 
 
 function javascript() {
     return src(paths.js)
-      .pipe(sourcemaps.init())
-      .pipe(concat('bundle.js')) // final output file name
-      .pipe(terser())
-      .pipe(sourcemaps.write('.'))
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./build/js'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js')) // final output file name
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('./build/js'))
 }
 
 function imagenes() {
     return src(paths.imagenes)
-        .pipe(cache(imagemin({ optimizationLevel: 3})))
+        .pipe(cache(imagemin({ optimizationLevel: 3 })))
         .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Imagen Completada'}));
+        .pipe(notify({ message: 'Imagen Completada' }));
 }
 
 function versionWebp() {
     return src(paths.imagenes)
-        .pipe( webp() )
+        .pipe(webp())
         .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Imagen Completada'}));
+        .pipe(notify({ message: 'Imagen Completada' }));
 }
 
 
 function watchArchivos() {
-    watch( paths.scss, css );
-    watch( paths.js, javascript );
-    watch( paths.imagenes, imagenes );
-    watch( paths.imagenes, versionWebp );
+    watch(paths.scss, css);
+    watch(paths.js, javascript);
+    watch(paths.imagenes, imagenes);
+    watch(paths.imagenes, versionWebp);
 }
-  
-exports.default = parallel(css, javascript,  imagenes, versionWebp, watchArchivos ); 
+
+exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos);
